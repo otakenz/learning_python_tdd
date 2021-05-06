dcb3b8a240048dee0bac7fc725fbad9fac9ced96
Otakenz <e0318537@u.nus.edu>
2021-05-07 00:28:47 +0800

    test

--- blog/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# File              : __init__.py
-# Author            : leejhunhong <jhunhong.lee@outlook.com>
-# Date              : 01.05.2021
-# Last Modified Date: 01.05.2021
-# Last Modified By  : leejhunhong <jhunhong.lee@outlook.com>
